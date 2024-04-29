5bb4316409a98fb0b6da09a056a8f6acaa0ca7ba
rubendc <rubendcdev@proton.me>
2024-04-29 16:20:25 +0200

    Ajout du composant App.vue et du layout par défaut

---
+++ layouts/default.vue
@@ -0,0 +1,157 @@
+<script setup lang="ts">
+	const links = [
+		{
+			href: '/',
+			iconName: 'akar-icons:home',
+			name: 'Accueil',
+		},
+		{
+			href: '/',
+			iconName: 'lucide:lightbulb',
+			name: 'Découvrir',
+		},
+		{
+			href: '/',
+			iconName: 'lucide:clapperboard',
+			name: 'Films',
+		},
+		{
+			href: '/',
+			iconName: 'lucide:tv',
+			name: 'Séries',
+		},
+	];
+</script>
+
+<template>
+	<div>
+		<header>
+			<h1 class="logo">
+				<NuxtImg class="icon" src="/popcorn.png" />
+
+				Cinémotion
+			</h1>
+
+			<nav class="nav">
+				<NuxtLink class="nav__link" to="/" v-for="link in links">
+					<Icon :name="link.iconName" />
+
+					{{ link.name }}
+				</NuxtLink>
+			</nav>
+		</header>
+
+		<main>
+			<slot />
+		</main>
+
+		<footer class="infos">
+			<div class="column">
+				<p>
+					Développé avec
+
+					<NuxtLink to="https://nuxt.com">
+						<Icon name="vscode-icons:file-type-nuxt" />
+						Nuxt JS
+					</NuxtLink>
+				</p>
+
+				<p>
+					Données fournies par
+
+					<NuxtLink to="https://themoviedb.org">
+						<IconsTmdb class="icon" />
+					</NuxtLink>
+				</p>
+
+				<p>
+					Ce projet utilise l'API TMDB mais n'est ni approuvé ni
+					certifié par TMDB.
+				</p>
+			</div>
+
+			<div class="column">
+				<NuxtLink
+					to="https://github.com/ruben-dc/cinemotion"
+					target="_blank"
+				>
+					<Icon name="lucide:external-link" />
+					repo github
+				</NuxtLink>
+
+				<p>designé avec Figma</p>
+			</div>
+		</footer>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+	header {
+		position: sticky;
+		top: 0;
+		left: 0;
+
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+
+		width: 100%;
+		padding: 30px 50px;
+
+		background: #00000010;
+		backdrop-filter: blur(40px);
+
+		.logo {
+			display: flex;
+			align-items: center;
+			gap: 10px;
+
+			font-family: 'Playfair Display';
+			font-weight: 500;
+			font-size: 26px;
+
+			.icon {
+				aspect-ratio: 1/1;
+				width: 40px;
+			}
+		}
+
+		.nav {
+			display: flex;
+			align-items: center;
+			gap: 30px;
+
+			&__link {
+				display: flex;
+				align-items: center;
+				gap: 5px;
+			}
+		}
+	}
+
+	main {
+		width: 100%;
+		min-height: 100vh;
+	}
+
+	footer {
+		display: flex;
+		gap: 60px;
+
+		width: 100%;
+		padding: 30px 50px;
+
+		border-top: solid 1px $strokes;
+
+		.column {
+			display: flex;
+			flex-direction: column;
+			gap: 10px;
+		}
+
+		.icon {
+			height: 10px;
+		}
+	}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div class="discover">
+		a
+		<div class="discover__image"></div>
+	</div>
+
+	<div class="search-bar">
+		<input type="search" placeholder="Chercher un film" />
+	</div>
+
+	<div class="content">test</div>
+</template>
+
+<style lang="scss" scoped>
+	.discover {
+		width: 100%;
+		height: 650px;
+
+		// box-shadow: 0 1000px 100px rgb(255, 0, 0);
+
+		&__image {
+			width: 100%;
+			height: 100%;
+
+			background: url(https://www.syfy.com/sites/syfy/files/2022/07/untitled-1_9.jpg;);
+			background-repeat: no-repeat;
+			background-size: cover;
+
+			box-shadow: inset 0 -450px 170px -200px #000000;
+		}
+	}
+
+	.content {
+		width: 100%;
+		max-width: 900px;
+	}
+</style>
