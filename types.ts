86bafe1b4f7f1ef8de3328c7c5fcb5cb5ce572c0
rubendc <rubendcdev@proton.me>
2024-05-06 16:22:39 +0200

    ajout du type DetailsRawDatas et mise à jour du type RawDatas

--- types.ts
+++ types.ts
@@ -2,9 +2,88 @@ export type MediaType = 'movie' | 'tv';
 
 export type RawDatas = {
 	page: number;
-	results: object[];
+	results: Movie[] | TV[];
 	total_pages: number;
 	total_results: number;
+};
+
+export type DetailsRawDatas = {
+	adult: boolean;
+	backdrop_path: string;
+	belongs_to_collection: {
+		id: number;
+		name: string;
+		poster_path: string;
+		backdrop_path: string;
+	};
+	budget: number;
+	// genres: [
+	// {
+	// 	"id": 27,
+	// 	"name": "Horror"
+	//   },
+	//   {
+	// 	"id": 53,
+	// 	"name": "Thriller"
+	//   }
+	// ];
+	homepage: string;
+	id: number;
+	imdb_id: string;
+	origin_country: string[];
+	original_language: string;
+	original_title: string;
+	overview: string;
+	popularity: number;
+	poster_path: string;
+	// production_companies: [
+	// 	{
+	// 		id: 143049;
+	// 		logo_path: null;
+	// 		name: 'Halloween VI Productions';
+	// 		origin_country: '';
+	// 	},
+	// 	{
+	// 		id: 21438;
+	// 		logo_path: null;
+	// 		name: 'Nightfall Productions';
+	// 		origin_country: '';
+	// 	},
+	// 	{
+	// 		id: 15822;
+	// 		logo_path: '/Aodu1OPI0LHTWYr7Jlm4R2XY2og.png';
+	// 		name: 'Trancas International Films';
+	// 		origin_country: 'US';
+	// 	},
+	// 	{
+	// 		id: 7405;
+	// 		logo_path: '/rfnws0uY8rsNAsrLbx3gEIcYXx3.png';
+	// 		name: 'Dimension Films';
+	// 		origin_country: 'US';
+	// 	}
+	// ];
+	// production_countries: [
+	// 	{
+	// 		iso_3166_1: 'US';
+	// 		name: 'United States of America';
+	// 	}
+	// ];
+	release_date: string;
+	revenue: number;
+	runtime: number;
+	// spoken_languages: [
+	// 	{
+	// 		english_name: 'English';
+	// 		iso_639_1: 'en';
+	// 		name: 'English';
+	// 	}
+	// ];
+	status: string;
+	tagline: string;
+	title: string;
+	video: false;
+	vote_average: number;
+	vote_count: number;
 };
 
 export type MediaInfos = {
