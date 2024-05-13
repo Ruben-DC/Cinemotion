c1edc8fef83788555537bc22484705c87be34952
Ruben DC <rubendcdev@proton.me>
2024-05-14 01:50:24 +0200

    modifications sur l'endpoint des listes TV pour mermettre la pagination. Fonctionel

--- server/api/tmdb/tv/[...list].ts
+++ server/api/tmdb/tv/[...list].ts
@@ -1,26 +1,34 @@
 import { RawDatas, TV } from '~/types';
-import { formatMediasArray } from '~/utils/format';
 import { fetchWithAuth } from '~/utils/fetch';
+import { formatMediasArray } from '~/utils/format';
 
 export default defineEventHandler(async (event) => {
 	const config = useRuntimeConfig(event);
 	const params = event.context.params?.list ?? '';
-	const tvsListUrl = `${config.public.baseUrl}/tv`;
+	const TvListUrl = `${config.public.baseUrl}/tv`;
 	const list = params?.split('/')[1];
+	const id = params?.split('/')[2] ? params.split('/')[2] : '';
+	const query = getQuery(event);
 
 	type RouteKey = keyof typeof routes;
 
 	const routes = {
-		airing: '/airing_today?language=fr-FR&page=1',
-		popular: '/popular?language=fr-FR&page=1',
-		top: '/top_rated?language=fr-FR&page=1',
-		ontheair: '/on_the_air?language=fr-FR&page=1',
+		airing: '/airing_today?language=fr-FR',
+		popular: '/popular?language=fr-FR',
+		top: '/top_rated?language=fr-FR',
+		ontheair: '/on_the_air?language=fr-FR',
+		reco: '/recommendations?language=fr-FR&page=1',
+		similar: '/similar?language=fr-FR&page=1',
 	};
 
 	const data: RawDatas = await fetchWithAuth(
-		`${tvsListUrl}/${routes[list as RouteKey]}`,
+		`${TvListUrl}/${id ? id + '/' : ''}${routes[list as RouteKey]}${
+			query.page ? `&page=${query.page}` : '&page=1'
+		}`,
 		config.tmdbApiKey
 	);
+
+	data.mediaType = 'tvs';
 
 	return formatMediasArray(data.results as TV[]);
 });
