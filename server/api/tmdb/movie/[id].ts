3b633903bbfdf4f7ab83d7daefa40e1f4b31d4c8
Ruben DC <rubendcdev@proton.me>
2024-05-13 22:30:35 +0200

    requête et typage d'un film en particulier

--- server/api/tmdb/movie/[id].ts
+++ server/api/tmdb/movie/[id].ts
@@ -1,4 +1,4 @@
-import { DetailsRawDatas } from '~/types';
+import { MovieDetails } from '~/types';
 import { fetchWithAuth } from '~/utils/fetch';
 
 export default defineEventHandler(async (event) => {
@@ -6,8 +6,8 @@ export default defineEventHandler(async (event) => {
 	const id = event.context.params?.id;
 	const baseUrl = `${config.public.baseUrl}/movie`;
 
-	const data: DetailsRawDatas = await fetchWithAuth(
-		`${baseUrl}/${id}`,
+	const data: MovieDetails = await fetchWithAuth(
+		`${baseUrl}/${id}?language=fr-FR`,
 		config.tmdbApiKey
 	);
 
