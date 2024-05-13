058c6a71cc014f2800ec92b92ef09fdd80d15cd4
Ruben DC <rubendcdev@proton.me>
2024-05-13 11:57:30 +0200

    ajout du type TvDetails et modification du type MovieDetails

--- types.ts
+++ types.ts
@@ -7,7 +7,7 @@ export type RawDatas = {
 	total_results: number;
 };
 
-export type DetailsRawDatas = {
+export type MovieDetails = {
 	adult: boolean;
 	backdrop_path: string;
 	belongs_to_collection: {
@@ -17,16 +17,11 @@ export type DetailsRawDatas = {
 		backdrop_path: string;
 	};
 	budget: number;
-	// genres: [
-	// {
-	// 	"id": 27,
-	// 	"name": "Horror"
-	//   },
-	//   {
-	// 	"id": 53,
-	// 	"name": "Thriller"
-	//   }
-	// ];
+	genres: {
+		id: number;
+		name: string;
+	}[];
+
 	homepage: string;
 	id: number;
 	imdb_id: string;
@@ -36,52 +31,119 @@ export type DetailsRawDatas = {
 	overview: string;
 	popularity: number;
 	poster_path: string;
-	// production_companies: [
-	// 	{
-	// 		id: 143049;
-	// 		logo_path: null;
-	// 		name: 'Halloween VI Productions';
-	// 		origin_country: '';
-	// 	},
-	// 	{
-	// 		id: 21438;
-	// 		logo_path: null;
-	// 		name: 'Nightfall Productions';
-	// 		origin_country: '';
-	// 	},
-	// 	{
-	// 		id: 15822;
-	// 		logo_path: '/Aodu1OPI0LHTWYr7Jlm4R2XY2og.png';
-	// 		name: 'Trancas International Films';
-	// 		origin_country: 'US';
-	// 	},
-	// 	{
-	// 		id: 7405;
-	// 		logo_path: '/rfnws0uY8rsNAsrLbx3gEIcYXx3.png';
-	// 		name: 'Dimension Films';
-	// 		origin_country: 'US';
-	// 	}
-	// ];
-	// production_countries: [
-	// 	{
-	// 		iso_3166_1: 'US';
-	// 		name: 'United States of America';
-	// 	}
-	// ];
+	production_companies: {
+		id: number;
+		logo_path?: string;
+		name: string;
+		origin_country?: string;
+	}[];
+
+	production_countries: {
+		iso_3166_1: string;
+		name: string;
+	}[];
+
 	release_date: string;
 	revenue: number;
 	runtime: number;
-	// spoken_languages: [
-	// 	{
-	// 		english_name: 'English';
-	// 		iso_639_1: 'en';
-	// 		name: 'English';
-	// 	}
-	// ];
+	spoken_languages: {
+		english_name: string;
+		iso_639_1: string;
+		name: string;
+	}[];
+
 	status: string;
 	tagline: string;
 	title: string;
 	video: false;
+	vote_average: number;
+	vote_count: number;
+};
+
+export type TvDetails = {
+	adult: boolean;
+	backdrop_path: string;
+	created_by: {
+		id: number;
+		credit_id: string;
+		name: string;
+		gender: number;
+		profile_path: string;
+	}[];
+	episode_run_time: number[];
+	first_air_date: string;
+	genres: {
+		id: number;
+		name: string;
+	}[];
+	homepage: string;
+	id: number;
+	in_production: boolean;
+	languages: string[];
+	last_air_date: string;
+	last_episode_to_air: {
+		id: number;
+		name: string;
+		overview: string;
+		vote_average: number;
+		vote_count: number;
+		air_date: string;
+		episode_number: number;
+		production_code: string;
+		runtime: number;
+		season_number: number;
+		show_id: number;
+		still_path: string;
+	};
+	name: string;
+	next_episode_to_air: null;
+	networks: {
+		id: number;
+		logo_path: string;
+		name: string;
+		origin_country: string;
+	}[];
+	number_of_episodes: number;
+	number_of_seasons: number;
+	origin_country: string[];
+	original_language: string;
+	original_name: string;
+	overview: string;
+	popularity: number;
+	poster_path: string;
+	production_companies: {
+		id: number;
+		logo_path: string;
+		name: string;
+		origin_country: string;
+	}[];
+
+	production_countries: [
+		{
+			iso_3166_1: string;
+			name: string;
+		}[]
+	];
+	seasons: {
+		air_date: string;
+		episode_count: number;
+		id: number;
+		name: string;
+		overview: string;
+		poster_path: string;
+		season_number: number;
+		vote_average: number;
+	}[];
+	spoken_languages: [
+		{
+			english_name: string;
+			iso_639_1: string;
+			name: string;
+		}
+	];
+	status: string;
+	tagline: string;
+	type: string;
 	vote_average: number;
 	vote_count: number;
 };
